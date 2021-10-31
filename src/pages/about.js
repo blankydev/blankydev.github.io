e76f31dd6fca78ab6508635119b86c9b9115030d
Blanky <metzonikos@gmail.com>
2021-10-31 16:29:18 +0200

    mini update

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,7 @@ const About = () => {
     <Layout>
 
         <Head>
-            <title>Blanky Dev | Contact</title>
+            <title>Blanky Dev | About</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
 
